77a48e29570f6a8930def5dee3a84a887c4cabe6
Bernd Hegmanns <it@hegmanns.de>
2016-10-03 13:06:57 +0200

    new RS resource for adding a new person

--- src/main/java/org/javaee7/sample/PersonResource.java
+++ src/main/java/org/javaee7/sample/PersonResource.java
@@ -1,10 +1,15 @@
 package org.javaee7.sample;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * @author arungupta
@@ -16,15 +21,21 @@ public class PersonResource {
     PersonDatabase database;
 
     @GET
-    @Produces("application/xml")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Person[] get() {
         return database.currentList();
     }
 
     @GET
     @Path("{id}")
-    @Produces("application/xml")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Person get(@PathParam("id") int id) {
         return database.getPerson(id);
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public int add(@FormParam("personname") String personname){
+    	return database.addPerson(personname);
     }
 }
